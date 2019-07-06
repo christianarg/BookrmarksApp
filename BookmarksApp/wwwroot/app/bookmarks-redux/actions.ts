8a60286a43aaaabcfbb3dc81c1cf2e2caf95e238
Christian Rodriguez <christianarg@outlook.com>
2019-07-06 11:24:47 +0200

    ya me cansé por ahora

---
+++ BookmarksApp/wwwroot/app/bookmarks-redux/actions.ts
@@ -0,0 +1,17 @@
+﻿import { BookmarkModel, TagModel, AddOrEditTagResult, EditBookmark } from "./bookmark-model";
+
+export function addBookmark(tag: TagModel, bookmarkModel: BookmarkModel) {
+
+}
+
+export function addOrEditTag(addOrEditTagResult: AddOrEditTagResult, parentTag: TagModel) {
+
+}
+
+export function editBookmark(tag: TagModel, editedBookmark: EditBookmark) {
+
+}
+
+export function search(searchValue: string) {
+
+}
