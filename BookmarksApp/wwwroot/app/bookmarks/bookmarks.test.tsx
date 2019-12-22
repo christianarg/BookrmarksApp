f37fa2330ffba029418876d60b1fb5e2c44be74a
Christian Rodriguez <christianarg@gmail.com>
2019-12-22 19:10:13 +0100

    ya había fichero de tests xD la extensión del nuevo fichero estaba mal :P

--- BookmarksApp/wwwroot/app/bookmarks/bookmarks.test.tsx
+++ BookmarksApp/wwwroot/app/bookmarks/bookmarks.test.tsx
@@ -1,83 +1,198 @@
-﻿import * as Bookrmarks from './bookmarks';
+﻿import * as React from 'react';
+import * as Bookmarks from './bookmarks';
+import { TagsRoot } from './bookmarks';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
 
-describe('filterTests', () => {
-    test('filter', () => {
-        // ARRANGE
-        const tag1: Bookrmarks.TagModel = {
-            name: '.Net',
-            bookmarks: [
-                { name: 'download', url: 'https://dotnet.microsoft.com/download' }
-            ]
-        };
-        const tag2: Bookrmarks.TagModel = {
-            name: 'react',
-            bookmarks: [
-                { name: 'react docs', url: 'https://reactjs.org/docs/getting-started.html' }
-            ]
-        };
-        const tags = [tag1, tag2];
+test("Render root tag when no tags", () => {
+    // ARRANGE
+    // ACT
+    const tagsRoot = shallow(<TagsRoot />);
 
-        // ACT
-        Bookrmarks.filterTags([tag1, tag2], 'down');
+    // ASSERT
+    expect(shallowToJson(tagsRoot)).toMatchSnapshot();
+});
 
-        // ARRANGE
-        expect(tag1.hidden).toBe(false);
-        expect(tag1.bookmarks[0].hidden).toBe(false);
+test("Render root tag when has tags", () => {
+    // ARRANGE
+    const tagsRoot = shallow(<TagsRoot />);
+    // ACT
 
-        expect(tag2.hidden).toBe(true);
-        expect(tag2.bookmarks[0].hidden).toBe(true);
+    // ASSERT
+});
 
-    });
-})
 
-describe('replaceTagTests', () => {
-    test('replaceTag', () => {
-        // ARRANGE
-        const newTag: Bookrmarks.TagModel = {
-            name: '.Net',
-            bookmarks: [{ name: 'download', url: 'https://dotnet.microsoft.com/download' }]
-        }
-        const tag1: Bookrmarks.TagModel = {
-            name: '.Net',
-            bookmarks: []
-        };
-        const tag2: Bookrmarks.TagModel = {
-            name: 'react',
-            bookmarks: []
-        };
-        const tags = [tag1, tag2];
-        // ACT
-        const replacedTags = Bookrmarks.replaceTag(tags, newTag);
 
-        // ASSERT
-        var newTagFromReplaced = replacedTags.find(tag => tag.name == newTag.name);
-        expect(newTagFromReplaced.bookmarks[0].name).toBe(newTag.bookmarks[0].name);
-    });
+test("Add root tag", () => {
+    // ARRANGE
 
-    test('replaceSubTag', () => {
-        // ARRANGE
-        const newTag: Bookrmarks.TagModel = {
-            name: 'Asp.Net',
-            bookmarks: [ { name: 'asp.net', url: 'https://dotnet.microsoft.com/apps/aspnet' }]
-        }
+    // ACT
 
-        const subTag: Bookrmarks.TagModel = {
-            name: 'Asp.Net',
-            bookmarks: []
-        };
+    // ASSERT
+});
 
-        const tag1: Bookrmarks.TagModel = {
-            name: '.Net',
-            bookmarks: [],
-            subTags: [subTag]
-        };
-        
-        const tags = [tag1];
-        // ACT
-        const replacedTags = Bookrmarks.replaceTag(tags, newTag);
+test("Remove root tag", () => {
+    // ARRANGE
 
-        // ASSERT
-        var newTagFromReplaced = replacedTags[0].subTags.find(tag => tag.name == newTag.name);
-        expect(newTagFromReplaced.bookmarks[0].name).toBe(newTag.bookmarks[0].name);
-    });
-})
+    // ACT
+
+    // ASSERT
+});
+
+test("Edit root tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Render nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+
+test("Add nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Remove nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Edit nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Render bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+
+test("Add bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Remove bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Edit bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+describe("helerFunctionTests", () => {
+    describe('filterTests', () => {
+        test('filter', () => {
+            // ARRANGE
+            const tag1: Bookmarks.TagModel = {
+                name: '.Net',
+                bookmarks: [
+                    { name: 'download', url: 'https://dotnet.microsoft.com/download' }
+                ]
+            };
+            const tag2: Bookmarks.TagModel = {
+                name: 'react',
+                bookmarks: [
+                    { name: 'react docs', url: 'https://reactjs.org/docs/getting-started.html' }
+                ]
+            };
+            const tags = [tag1, tag2];
+
+            // ACT
+            Bookmarks.filterTags([tag1, tag2], 'down');
+
+            // ARRANGE
+            expect(tag1.hidden).toBe(false);
+            expect(tag1.bookmarks[0].hidden).toBe(false);
+
+            expect(tag2.hidden).toBe(true);
+            expect(tag2.bookmarks[0].hidden).toBe(true);
+
+        });
+    })
+
+    describe('replaceTagTests', () => {
+        test('replaceTag', () => {
+            // ARRANGE
+            const newTag: Bookmarks.TagModel = {
+                name: '.Net',
+                bookmarks: [{ name: 'download', url: 'https://dotnet.microsoft.com/download' }]
+            }
+            const tag1: Bookmarks.TagModel = {
+                name: '.Net',
+                bookmarks: []
+            };
+            const tag2: Bookmarks.TagModel = {
+                name: 'react',
+                bookmarks: []
+            };
+            const tags = [tag1, tag2];
+            // ACT
+            const replacedTags = Bookmarks.replaceTag(tags, newTag);
+
+            // ASSERT
+            var newTagFromReplaced = replacedTags.find(tag => tag.name == newTag.name);
+            expect(newTagFromReplaced.bookmarks[0].name).toBe(newTag.bookmarks[0].name);
+        });
+
+        test('replaceSubTag', () => {
+            // ARRANGE
+            const newTag: Bookmarks.TagModel = {
+                name: 'Asp.Net',
+                bookmarks: [{ name: 'asp.net', url: 'https://dotnet.microsoft.com/apps/aspnet' }]
+            }
+
+            const subTag: Bookmarks.TagModel = {
+                name: 'Asp.Net',
+                bookmarks: []
+            };
+
+            const tag1: Bookmarks.TagModel = {
+                name: '.Net',
+                bookmarks: [],
+                subTags: [subTag]
+            };
+
+            const tags = [tag1];
+            // ACT
+            const replacedTags = Bookmarks.replaceTag(tags, newTag);
+
+            // ASSERT
+            var newTagFromReplaced = replacedTags[0].subTags.find(tag => tag.name == newTag.name);
+            expect(newTagFromReplaced.bookmarks[0].name).toBe(newTag.bookmarks[0].name);
+        });
+    })
+});
