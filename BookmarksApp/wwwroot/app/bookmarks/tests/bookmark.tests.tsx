606937d9f26605f6ba35a5a0f2a42925325343d3
Christian Rodriguez <christianarg@outlook.com>
2019-12-22 15:59:05 +0100

    esqueleto de tests

---
+++ BookmarksApp/wwwroot/app/bookmarks/tests/bookmark.tests.tsx
@@ -0,0 +1,75 @@
+﻿import * as React from 'react';
+
+test("Add root tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Remove root tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Edit root tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Add nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Remove nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Edit nested tag", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+
+test("Add bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Remove bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
+test("Edit bookmark", () => {
+    // ARRANGE
+
+    // ACT
+
+    // ASSERT
+});
+
