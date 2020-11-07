55ec93f1fba0222bb57584aabf1da0b76d7754c7
Christian Rodriguez <christianarg@outlook.com>
2020-11-07 13:08:08 +0100

    finalmente dios entiendo todo esto

--- BookmarksApp/app/jestsamples/mockfunction/mockfunction.test.ts
+++ BookmarksApp/app/jestsamples/mockfunction/mockfunction.test.ts
@@ -1,12 +1,13 @@
 import { sampleFunction1 } from "./module-with-function";
 jest.mock("./module-with-function");
 
-const sampleFunction1Mock = sampleFunction1 as unknown as jest.Mock;
+const sampleFunction1Mock = sampleFunction1 as unknown as jest.Mock<ReturnType<typeof sampleFunction1>>;
 
 // npx jest module-with -t "simple sample function test"
 test('simple sample function test', () =>{
     // ARRANGE
     const mockedReturnValue = "Bye";
+    
     sampleFunction1Mock.mockReturnValue(mockedReturnValue);
     
     // ACT
